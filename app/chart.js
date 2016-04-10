5e813ede15a8dc3502d7911fb0a452987a25ef91
Jonathan Huang <jonwhuang@gmail.com>
2016-04-10 12:07:30 -0700

    Add transcript box and input box rounded edges

--- app/chart.js
+++ app/chart.js
@@ -16,6 +16,7 @@ app.controller("chartCtrl", ['$scope', '$http', 'Upload', 'urlService', function
   $scope.data = [
   [0]
   ];
+
   var sampleText = [
   "He asked me a few questions about my object in coming, my antecedents generally, the mode in which I afterwards proposed to use the knowledge I might acquire, and finally, whether I wished to study any special branch. To the latter I replied that while I wished to be well grounded in all departments of zoology, I purposed to devote myself specially to insects. 'When do you wish to begin?' he asked. 'Now,' I replied. This seemed to please him, and with an energetic 'Very well,' he reached from a shelf a huge jar of specimens in yellow alcohol. ",
   "'Take this fish,' said he, 'and look at it; we call it a haemulon; by and by I will ask what you have seen.' With that he left me, but in a moment returned with explicit instructions as to the care of the object entrusted to me. 'No man is fit to be a naturalist,' said he, 'who does not know how to take care of specimens.' I was to keep the fish before me in a tin tray,and occasionally moisten the surface with alcohol from the jar, always taking care to replace the stopper tightly. Those were not the days of ground glass stoppers, and elegantly shaped exhibition jars; all the old students will recall the huge, neckless glass bottles with their leaky, wax-besmeared corks, half eaten by insects and begrimed with cellar dust.",
@@ -25,6 +26,8 @@ app.controller("chartCtrl", ['$scope', '$http', 'Upload', 'urlService', function
   "He listened attentively to my brief rehearsal of the structure of parts whose names were still unknown to me; the fringed gill-arches and movable operculum; the pores of the head, fleshy lips and lidless eyes; the lateral line, the spinous fins, and forked tail; the compressed and arched body. When I had finished, he waited as if expecting more, and then, with an air of disappointment: 'You have not looked very carefully; why,' he continued, more earnestly, 'you haven't even see one of the most conspicuous features of the animal, which is as plainly before your eyes as the fish itself; look again, look again!' and he left me to my misery. I was piqued; I was mortified. Still more of that wretched fish! But now I set myself to my task with a will, and discovered one new thing after another, until I saw how just the professor's criticism had been. The afternoon passed quickly, and when, towards its close, the professor inquired: 'Do you see it yet?' 'No,' I replied, 'I am certain I do not, but I see how little I saw before.' 'That is the next best,' said he earnestly, 'but I won't hear you now; put away your fish and go home; perhaps you will be ready with a better answer in the morning.",
   "I will examine you before you look at the fish.' This was disconcerting; not only must I think of my fish all night, studying without the object before me, what this unknown but most visible feature might be; but also, without reviewing my new discoveries, I must give an exact account of them the next day. I had a bad memory; so I walked home by the Charles River in a distracted state, with my two perplexities. The cordial greeting from the professor the next morning was reassuring; here was a man who seemed to be quite as anxious as I that I should see for myself what he saw. 'Do you perhaps mean,' I asked, 'that the fish has symmetrical sides with paired organs?' His thoroughly pleased 'Of course! of course!' repaid the wakeful hours of the previous night. After he had discoursed most happily and enthusiastically--as he always did--upon the importance of this point, I ventured to ask what I should do next. 'Oh, look at your fish!' he said, and left me again to my own devices."
   ];
+
+  $scope.sampleText = sampleText.join(' ');
 
   sampleText.forEach(function(text){
     calculateSentiment(text);
@@ -130,7 +133,7 @@ app.controller("chartCtrl", ['$scope', '$http', 'Upload', 'urlService', function
 
   $scope.upload = function(file) {
     Upload.upload({
-      url: "https://api.havenondemand.com/1/api/async/recognizespeech/v1", 
+      url: "https://api.havenondemand.com/1/api/async/recognizespeech/v1",
       data: {file:file[0], apikey:"4b212618-5f67-4f0d-b63a-45233c145396"}
     })
     .error(function(response){
