548d5cb2e723ed629ee9544b1cc572b1a9ae1220
Jonathan Huang <jonwhuang@gmail.com>
2016-04-09 20:06:05 -0700

    Start converting jquery to angular

--- app/chart.js
+++ app/chart.js
@@ -1,4 +1,8 @@
-app.controller("chartCtrl", function ($scope) {
+app.controller("chartCtrl", ['$scope', '$http', function ($scope, $http) {
+
+  // $scope.labels = [];
+
+  // $scope.data = [];
 
   $scope.labels = [":10",":20",":30",":40",":50","1:00", "1:10","1:20","1:30"],
 
@@ -6,21 +10,22 @@ app.controller("chartCtrl", function ($scope) {
     [-0.8294462782412093, -0.7575233110696172, -0.3573443110696172, -0.6441003110696172, 0.7570003489696172, -0.4324313110696172, -0.6234003110696172, 0.1570003110696172,1]
   ];
 
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
+  // $scope.onClick = function (points, evt) {
+  //   console.log(points, evt);
+  // };
+
+  // $scope.onHover = function(points) {
+  //   if (points.length > 0) {
+  //       console.log('Point', points[0].value);
+  //     } else {
+  //       console.log('No point');
+  //     };
+  // };
+
+  $scope.chartOptions = {
+    bezierCurve: false
   };
 
-  $scope.onHover = function(points) {
-    if (points.length > 0) {
-        console.log('Point', points[0].value);
-      } else {
-        console.log('No point');
-      };
-  };
-
-$scope.chartOptions = {
-  bezierCurve: false
-};
 
   var avergeArray = $scope.data[0]
   var total = 0;
@@ -30,4 +35,39 @@ $scope.chartOptions = {
   var avg = total / avergeArray.length
   $scope.average = avg
 
-});
+  $scope.testSpeechRecognition = function(){
+    audio_url = document.getElementById('inputLink').value;
+    console.log(audio_url);
+    $.ajax({
+        method: 'post',
+        url: "https://api.havenondemand.com/1/api/async/recognizespeech/v1",
+        data: {url:audio_url,apikey:"4b212618-5f67-4f0d-b63a-45233c145396"}
+    }).done(function(response){
+        console.log(response.jobID);
+        console.log('Making Job Request..')
+        $.ajax({
+            method: 'get',
+            url: "https://api.havenondemand.com/1/job/result/" + response.jobID +"?apikey=4b212618-5f67-4f0d-b63a-45233c145396"
+        }).done(function(response){
+            console.log("Received job request..now pinging sentiment...")
+            console.log(response);
+            var string = response.actions[0].result.document[0].content;
+            $('#result').html("String: "+string);
+            $.ajax({
+                method: 'post',
+                url: "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1",
+                data: {apikey:'4b212618-5f67-4f0d-b63a-45233c145396',language:'eng',text:string}
+            }).done(function(response){
+                $('#result').append("<br>Sentiment: " + response.aggregate.score)
+                console.log(response);
+
+            });
+        });
+    })
+  }
+
+
+
+
+
+}]);
